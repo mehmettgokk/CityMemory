898af72cacec3cb284bd8c8c3d3c052ff5030d28
mgok0712-glitch <mgok0712@gmail.com>
2026-09-11 14:59:40 +0300

    feat: add nominatim composable for map search API

---
+++ app/composables/useNominatim.ts
@@ -0,0 +1,58 @@
+import { ref } from 'vue'
+import type { Place } from '~/types/place'
+
+export const useNominatim = () => {
+
+    const searchResults = ref<Place[]>([])
+    const isLoading = ref(false)
+    const error = ref<string | null>(null)
+
+    const transformToPlace = (item: any): Place => {
+        return {
+            id: item.place_id.toString(),
+            name: item.name || (item.display_name ? item.display_name.split(',')[0] : 'Bilinmeyen Yer'),
+            category: item.type || item.class || 'unknown',
+            latitude: parseFloat(item.lat),
+            longitude: parseFloat(item.lon),
+            address: item.display_name
+        }
+    }
+    
+    const search = async (query: string) => {
+        if(!query.trim()){
+            searchResults.value = []
+            return
+        }
+    
+        isLoading.value = true
+        error.value = null
+
+        try {
+            const response = await $fetch<any[]>('https://nominatim.openstreetmap.org/search',{
+                params: {
+                    q: query,
+                    format: 'json',
+                    addressdetais: 1,
+                    limit: 10
+                },
+                headers: {
+                    'Accept-Language': 'tr-TR'
+                }
+            })
+
+          searchResults.value = response.map(transformToPlace)
+        } catch(err){
+          error.value = 'Arama yapılırken bir hata oluştu: Lütfen bağlantınızı kontrol edin'
+          console.error(err)
+        } finally {
+            isLoading.value = false
+        }
+    }
+
+    return {
+        search,
+        searchResults,
+        isLoading,
+        error
+    }
+}
